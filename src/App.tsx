5dea573d9bce4ba54a769e23cb163ad89856a0da
joaolucasraday <lucasraday@gmail.com>
2026-01-23 19:38:26 -0300

    SafeAreaView and StatusBar Styles

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import { AppRoutes } from './AppRoutes';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Theme } from './shared/themes/Theme';
+import { StatusBar } from 'expo-status-bar';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -23,5 +26,10 @@ export function App() {
 
   if (!loaded && !error) return null;
 
-  return <AppRoutes/>;
+  return (
+    <SafeAreaView style={{flex:1, backgroundColor: Theme.colors.background}}>
+      <StatusBar style='light'/>
+      <AppRoutes/>
+    </SafeAreaView>
+  );
 }
