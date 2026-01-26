e95f573d0724c6b271ae062d29e44f0f0d43a17c
joaolucasraday <lucasraday@gmail.com>
2026-01-26 10:00:42 -0300

    buttons stylesheet finished

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useNavigation } from "@react-navigation/native"
-import { View, Text, TouchableOpacity } from "react-native"
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native"
 import { TNavigationScreenProps } from "../AppRoutes";
 import { Theme } from "../shared/themes/Theme";
 
@@ -8,11 +8,59 @@ export const Home = () => {
     const navigation = useNavigation<TNavigationScreenProps>();
     return(
         <View>
-            <Text style={{fontFamily: 'InterRegular', fontSize:52, color: Theme.colors.text}}>Home Page</Text>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.primaryButton}>
+                    <Text style={styles.primaryButtonText}>Iniciar</Text>
+                </TouchableOpacity>
+            </View>
+                        <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.primaryButton}>
+                    <Text style={styles.primaryButtonText}>Pausar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.secondaryButton}>
+                    <Text style={styles.secondaryButtonText}>Parar</Text>
+                </TouchableOpacity>
+            </View>
+                        <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.primaryButton}>
+                    <Text style={styles.primaryButtonText}>Continuar</Text>
+                </TouchableOpacity>
 
-            <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
-                <Text style={{ color: Theme.colors.text}}>Settings</Text>
-            </TouchableOpacity>
+                <TouchableOpacity style={styles.secondaryButton}>
+                    <Text style={styles.secondaryButtonText}>Reniciar</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    primaryButton: {
+        backgroundColor: Theme.colors.primary,
+        padding: 16,
+        paddingHorizontal: 24,
+        borderRadius: 55,
+    },
+    primaryButtonText: {
+        color: Theme.colors.text,
+        fontFamily: Theme.fonts.interRegular,
+        fontSize: Theme.fontSizes.body
+    },
+    secondaryButton: {
+        borderColor: Theme.colors.primary,
+        borderWidth: 2,
+        padding: 16,
+        paddingHorizontal: 24,
+        borderRadius: 55,
+    },
+    secondaryButtonText: {
+        color: Theme.colors.text,
+        fontFamily: Theme.fonts.interRegular,
+        fontSize: Theme.fontSizes.body
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        gap: 16
+    }
+})
