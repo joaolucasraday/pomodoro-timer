c2e554d02885caa71f94d09a308997306492b0a9
joaolucasraday <lucasraday@gmail.com>
2026-01-29 19:54:48 -0300

    dynamic buttons config

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -3,9 +3,16 @@ import { MaterialIcons } from '@expo/vector-icons'
 import { Theme } from "../shared/themes/Theme"
 import { useNavigation } from "@react-navigation/native"
 import { TNavigationScreenProps } from "../AppRoutes";
+import { useState } from "react";
 
 export const Settings = () => {
     const navigation = useNavigation<TNavigationScreenProps>();
+
+    const [focusPeriod, setFocusPeriod] = useState(25);
+    const [shortBreakPeriod, setShortBreakPeriod] = useState(5)
+    const [longBreakPeriod, setLongBreakPeriod] = useState(15)
+    const [notificationsPeriod, setNotificationsPeriod] = useState(true)
+
     return(
         <View style={styles.mainContainer}>
                 <TouchableOpacity style={styles.closeButton}onPress={() => navigation.navigate('Home')}>
@@ -22,13 +29,19 @@ export const Settings = () => {
                         <View style={styles.pausasContainer}>
                             <Text style={styles.titleGroup}>Período de foco</Text>
                             <View style={styles.pausaButtons}>
-                                <TouchableOpacity style={styles.button}>
+                                <TouchableOpacity 
+                                style={focusPeriod === 15 ? styles.activeButton : styles.button} 
+                                onPress={() => setFocusPeriod(15)}>
                                     <Text style={styles.buttonText}>15 min</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity style={styles.activeButton}>
+                                <TouchableOpacity 
+                                style={focusPeriod === 25 ? styles.activeButton : styles.button} 
+                                onPress={() => setFocusPeriod(25)}>
                                     <Text style={styles.buttonText}>25 min</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity style={styles.button}>
+                                <TouchableOpacity 
+                                style={focusPeriod === 35 ? styles.activeButton : styles.button} 
+                                onPress={() => setFocusPeriod(35)}>
                                     <Text style={styles.buttonText}>35 min</Text>
                                 </TouchableOpacity>
                             </View>
@@ -36,13 +49,19 @@ export const Settings = () => {
                         <View style={styles.pausasContainer}>
                             <Text style={styles.titleGroup}>Pausa curta</Text>
                             <View style={styles.pausaButtons}>
-                                <TouchableOpacity style={styles.activeButton}>
+                                <TouchableOpacity 
+                                style={shortBreakPeriod === 3 ? styles.activeButton : styles.button}
+                                onPress={() => setShortBreakPeriod(3)}>
                                     <Text style={styles.buttonText}>3 min</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity style={styles.button}>
+                                <TouchableOpacity 
+                                style={shortBreakPeriod === 5 ? styles.activeButton : styles.button}
+                                onPress={() => setShortBreakPeriod(5)}>
                                     <Text style={styles.buttonText}>5 min</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity style={styles.button}>
+                                <TouchableOpacity 
+                                style={shortBreakPeriod === 7 ? styles.activeButton : styles.button}
+                                onPress={() => setShortBreakPeriod(7)}>
                                     <Text style={styles.buttonText}>7 min</Text>
                                 </TouchableOpacity>
                             </View>
@@ -50,13 +69,19 @@ export const Settings = () => {
                         <View style={styles.pausasContainer}>
                             <Text style={styles.titleGroup}>Pausa longa</Text>
                             <View style={styles.pausaButtons}>
-                                <TouchableOpacity style={styles.button}>
+                                <TouchableOpacity 
+                                style={longBreakPeriod === 10 ? styles.activeButton : styles.button}
+                                onPress={() => setLongBreakPeriod(10)}>
                                     <Text style={styles.buttonText}>10 min</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity style={styles.activeButton}>
+                                <TouchableOpacity 
+                                style={longBreakPeriod === 15 ? styles.activeButton : styles.button}
+                                onPress={() => setLongBreakPeriod(15)}>
                                     <Text style={styles.buttonText}>15 min</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity style={styles.button}>
+                                <TouchableOpacity 
+                                style={longBreakPeriod === 20 ? styles.activeButton : styles.button}
+                                onPress={() => setLongBreakPeriod(20)}>
                                     <Text style={styles.buttonText}>20 min</Text>
                                 </TouchableOpacity>
                             </View>
@@ -64,10 +89,14 @@ export const Settings = () => {
                         <View style={styles.pausasContainer}>
                             <Text style={styles.titleGroup}>Notificações</Text>
                             <View style={styles.pausaButtons}>
-                                <TouchableOpacity style={styles.button}>
+                                <TouchableOpacity 
+                                style={notificationsPeriod === false ? styles.activeButton : styles.button}
+                                onPress={() => setNotificationsPeriod(false)}>
                                     <Text style={styles.buttonText}>Desativado</Text>
                                 </TouchableOpacity>
-                                <TouchableOpacity style={styles.activeButton}>
+                                <TouchableOpacity 
+                                style={notificationsPeriod === true ? styles.activeButton : styles.button}
+                                onPress={() => setNotificationsPeriod(true)}>
                                     <Text style={styles.buttonText}>Ativado</Text>
                                 </TouchableOpacity>
                             </View>
